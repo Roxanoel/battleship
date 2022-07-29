9532eff945e93f62a4251c3805bd2e08e581526c
Roxanoel <roxane.noel92@gmail.com>
2022-07-29 14:04:04 +0100

    Wrote tests for basic functions of Player.

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,2 +1,27 @@
 import Player from "../src/player";
+import Gameboard from '../src/gameboard';
 
+describe('Generic Player functions', () => {
+    let player;
+
+    beforeEach(() => {
+        player = Player();
+    });
+
+    test('Player gets associated to their own game board', () => {
+        expect(player.getGameBoard()).toBeTruthy();
+    });
+
+    test('Player gets a name property', () => {
+        expect(player.hasOwn('name')).toBe(true);
+    });
+
+    test('Player has reference to opponent board', () => {
+        expect(player.hasOwn('opponentBoard')).toBe(true);
+    });
+
+    test('Opponent board can be set after player init', () => {
+        player.SetOpponentBoard(Gameboard());
+        expect(player.opponentBoard).toBeTruthy();
+    });
+});
