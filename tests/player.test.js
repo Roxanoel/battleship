36e98fecb742e28671b81a6e59be25cef49c31c9
Roxanoel <roxane.noel92@gmail.com>
2022-07-31 13:56:25 +0100

    Added some tests for playTurn function.

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,13 +1,13 @@
 import Player from "../src/player";
 import Gameboard from '../src/gameboard';
 
-describe('Generic Player functions', () => {
-    let player;
+let player;
 
     beforeEach(() => {
         player = Player();
     });
 
+describe('Generic Player functions', () => {
     test('Player gets associated to their own game board', () => {
         expect(player.getGameBoard()).toBeTruthy();
     });
@@ -23,5 +23,17 @@ describe('Generic Player functions', () => {
     test('Opponent board can be set after player init', () => {
         player.SetOpponentBoard(Gameboard());
         expect(player.opponentBoard).toBeTruthy();
+    });
+});
+
+describe('Playing a turn', () => {
+    beforeEach(() => {
+        player.SetOpponentBoard();
+    });
+    test('playTurn throws error if no coords are passed in', () => {
+        expect(player.playTurn()).toThrow();
+    });
+    test('playTurn throws error if coords are out of bounds', () => {
+        expect(player.playTurn(11, 11)).toThrow();
     });
 });
