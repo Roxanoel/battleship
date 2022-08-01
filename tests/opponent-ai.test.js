9b9cf02a8cf163c544492ad6884d99be5dd285df
Roxanoel <roxane.noel92@gmail.com>
2022-08-01 12:04:11 +0100

    Fixed problem with ai test code.

--- tests/opponent-ai.test.js
+++ tests/opponent-ai.test.js
@@ -19,12 +19,12 @@ describe('Making a random legal move', () => {
     });
 
     test('If a cell gets hit, it is not part of the legalMoves array: checking length', () => {
-        oppositeBoard.receiveAttack(0); // Attacks 0,0 aka index 0
+        oppositeBoard.receiveAttack(oppositeBoard.getCells()[0]); // Attacks 0,0 aka index 0
         expect(opponentAI.getLegalMoves().length).toBe(opponentAI.oppositeBoard.getCells().length - 1);
     });
 
     test('If a cell gets hit, it is not part of the legalMoves array: will not be hit twice', () => {
-        oppositeBoard.receiveAttack(0); // Attacks 0,0 aka index 0
+        oppositeBoard.receiveAttack(oppositeBoard.getCells()[0]); // Attacks 0,0 aka index 0
         const selectedCell = opponentAI.getLegalMoves[mockRandom()];
         expect(convertCoordinatesToIndex(selectedCell.x, selectedCell.y)).not.toBe(0);
     });
