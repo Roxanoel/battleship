c332408f0195c049f8008bb3ea1dbe7929e2021d
Roxanoel <roxane.noel92@gmail.com>
2022-08-03 10:53:02 +0100

    Wrote a shipspecs file. Contains array of all ships with names and sizes.

---
+++ src/shipspecs.js
@@ -0,0 +1,26 @@
+const shipSpecs = [
+    {
+        name: 'carrier',
+        size: 5,
+    },
+    {
+        name: 'battleship',
+        size: 4,
+    },
+    {
+        name: 'destroyer',
+        size: 3,
+    },
+    {
+        name: 'submarine',
+        size: 3,
+    },
+    {
+        name: 'patrol boat',
+        size: 2,
+    }
+];
+
+export {
+    shipSpecs,
+}
