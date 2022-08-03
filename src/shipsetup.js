fda374b95b45a6bde8217a435842da081eb71f6d
Roxanoel <roxane.noel92@gmail.com>
2022-08-03 11:29:56 +0100

    Wrote pseudocode in shipsetup. Wrote mocks for shipsetup tests.

--- src/shipsetup.js
+++ src/shipsetup.js
@@ -1,13 +1,21 @@
-async function placeShip(shipInfo) {
-
+async function placeShip(player, shipInfo) {
+    // read whether the player is ai, if the player is ai use the ai to place the ship
+    //await info for placement, would come from form in final app v1, no drag and drop yet
+    //then place ship at the right spot using ref to player
 }
 
 async function placeAllShipsRoutine(player) {
+    // for loop for placing each ship based on length of shipspecs
 
+        // inside loop: call placeShip and once it resolves, loop can continue... do while?
+
+    // once loop is done the promise is resolved
 }
 
 async function shipSetup(players) {
+    // for loop for each player 
 
+    // 
 }
 
 export {
--- tests/shipsetup.test.js
+++ tests/shipsetup.test.js
@@ -1,1 +1,33 @@
 import { placeShip, placeAllShipsRoutine, shipSetup } from "../src/shipsetup";
+import { Gameboard } from '../src/gameboard';
+
+let mockPlayer;
+
+beforeAll(() => {
+
+    mockPlayer = () => {
+        const playerBoard = Gameboard();
+        const opponentBoard = Gameboard();
+
+        return { name: 'Player',
+        ai: undefined,
+        getGameboard: () => { return playerBoard },
+        getOpponentBoard: () => {return opponentBoard },
+        }
+    };
+});
+
+describe('Placing individual ships', () => {
+    let mockShipData;
+    
+    beforeAll(() => {
+        mockShipData = {
+            name: 'test ship',
+            size: '3',
+        }
+
+
+    });
+
+    test('')
+});
