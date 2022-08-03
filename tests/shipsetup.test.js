50362d0ab4693d2fdbcbd3e6c5259c5fc7087728
Roxanoel <roxane.noel92@gmail.com>
2022-08-03 13:30:55 +0100

    Wrote first test to test mock.

--- tests/shipsetup.test.js
+++ tests/shipsetup.test.js
@@ -2,9 +2,6 @@ import { placeShip, placeAllShipsRoutine, shipSetup } from "../src/shipsetup";
 import { Gameboard } from '../src/gameboard';
 import { jest } from "@jest/globals";
 
-let mockPlayer;
-let mockOpponent;
-
 const mockOpponentAI = {
     getRandomShipPlacement: jest.fn(() => { return {x: 0, y: 0} }),
 }
@@ -13,44 +10,44 @@ const mockBoard = () => {
     jest.fn(() => Gameboard());
 }
 
-const mockPlayerPlacement = 
+function mockPlayer() {
+    const playerBoard = mockBoard;
+    const opponentBoard = mockBoard;
 
-beforeAll(() => {
-
-    mockPlayer = () => {
-        const playerBoard = mockBoard;
-        const opponentBoard = mockBoard;
-
-        return { name: 'Player',
-        ai: undefined,
-        getGameboard: () => { return playerBoard },
-        getOpponentBoard: () => {return opponentBoard },
-        }
-    };
-
-    mockOpponent = () => {
-        const playerBoard = mockBoard;
-        const opponentBoard = mockBoard;
-
-        return { name: 'Player',
-        ai: mockOpponentAI,
-        getGameboard: () => { return playerBoard },
-        getOpponentBoard: () => {return opponentBoard },
-        }
+    return { name: 'Player',
+    ai: undefined,
+    getGameboard: () => { return playerBoard },
+    getOpponentBoard: () => {return opponentBoard },
     }
-});
+};
+
+function mockOpponent() {
+    const playerBoard = mockBoard;
+    const opponentBoard = mockBoard;
+
+    return { name: 'Player',
+    ai: mockOpponentAI,
+    getGameboard: () => { return playerBoard },
+    getOpponentBoard: () => {return opponentBoard },
+    }
+}
 
 describe('Placing individual ships', () => {
     let mockShipData;
+    let humanPlayer;
+    let aiPlayer;
     
     beforeAll(() => {
         mockShipData = {
             name: 'test ship',
             size: '3',
         }
+        humanPlayer = mockPlayer();
+        aiPlayer = mockOpponent();
     });
 
     test('placeShip for AI calls the getRandomShipPlacement method', () => {
-
+        placeShip(mockOpponent, mockShipData);
+        console.log(aiPlayer.ai.getRandomShipPlacement.mock.calls);
     });
 });
